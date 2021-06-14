320fcb9ccc6b42445b716830673460c25be37c47
Patrick Ulmer <patrick.ulmer@campoint.net>
2021-06-14 20:53:06 +0200

    improve docker image tagging by using git tag semver and travis build number

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,13 @@
-docker build -t pulmer75/complex-client:latest -t pulmer75/complex-client:$GIT_SHA -f ./client/Dockerfile ./client
-docker build -t pulmer75/complex-server:latest -t pulmer75/complex-server:$GIT_SHA -f ./server/Dockerfile ./server
-docker build -t pulmer75/complex-worker:latest -t pulmer75/complex-worker:$GIT_SHA -f ./worker/Dockerfile ./worker
+docker build -t pulmer75/complex-client:latest -t pulmer75/complex-client:$APP_VERSION -f ./client/Dockerfile ./client
+docker build -t pulmer75/complex-server:latest -t pulmer75/complex-server:$APP_VERSION -f ./server/Dockerfile ./server
+docker build -t pulmer75/complex-worker:latest -t pulmer75/complex-worker:$APP_VERSION -f ./worker/Dockerfile ./worker
 
 docker push pulmer75/complex-client:latest
-docker push pulmer75/complex-client:$GIT_SHA
+docker push pulmer75/complex-client:$APP_VERSION
 docker push pulmer75/complex-server:latest
-docker push pulmer75/complex-server:$GIT_SHA
+docker push pulmer75/complex-server:$APP_VERSION
 docker push pulmer75/complex-worker:latest
-docker push pulmer75/complex-worker:$GIT_SHA
+docker push pulmer75/complex-worker:$APP_VERSION
 
 #kubectl apply -f k8s
 #kubectl set image deployments/client-deployment server=pulmer75/complex-client
